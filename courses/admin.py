8723bb497a8a5f8103ef65e24640b7643a01e481
kimplausible <andyk1278@gmail.com>
2019-04-03 13:00:06 -0700

    Added content abstract models for each file type.

--- courses/admin.py
+++ courses/admin.py
@@ -17,6 +17,6 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'subject', 'created']
     list_filter = ['created', 'subject']
     search_fields = ['title', 'overview']
-    prepopulated_fields = {'slug':('title',)}
+    prepopulated_fields = {'slug': ('title',)}
     inlines = [ModuleInline]
 
