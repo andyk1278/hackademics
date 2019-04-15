9700b0c382affe0b76f174f8a4aebffaab3e6ecc
kimplausible <andyk1278@gmail.com>
2019-04-14 23:14:23 -0700

    Created order OrderField field to Modules to add custom ordering to Module contents.

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from .fields import OrderField
+
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User,
@@ -71,9 +73,13 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order',]
 
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
 
 
 class Content(models.Model):
@@ -89,4 +95,8 @@ class Content(models.Model):
                                                        'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['modulem,'])
+    class Meta:
+        ordering = ['order']
+
 
